f4dffdea4e6553f7cd2cd70dbad3e4aad3e938f6
Usuario <usuario@MacBook-Pro-de-Heitor.local>
2022-06-04 01:07:32 -0300

    Card added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,7 @@ export function Sidebar(){
      <aside className={styles.sidebar}> 
         <img className={styles.cover} src='https://images.unsplash.com/photo-1526224617767-e92ff8a3fb71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' />
         <div className={styles.profile}>
+            <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/12464543?v=4"/>
             <strong>Gustavo Cabral</strong>
             <span>Web Developer</span>
         </div>
